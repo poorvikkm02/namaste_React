0aa7c0840c05840f88e67e1656fdb331f9f1ce9e
Poorvik K M <poorvik9683@gmail.com>
2026-03-09 15:01:52 +0530

    epidode-01

---
+++ app.js
@@ -0,0 +1,16 @@
+const heading = React.createElement("h1", {}, "Hello World frm namaste React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "Hi this is Parent nested div"),React.createElement("h2",{},"Hi this is h2")]
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
